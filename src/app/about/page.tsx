8185876d02ae6076b5488d8acce0874f0c66d4fe
sakr <sakr.bzns@gmail.com>
2024-02-19 16:54:36 +0200

    Image Component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image"
+import Logo from '../../../public/logo.png'
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <section className="min-h-[calc(100vh-150px)] ">
+      <h1 className="text-3xl font-bold text-gray-800 p-5">AboutPage</h1>
+      <div>
+        <Image src={Logo} alt="Logo-Image" width={500} height={500} priority/>
+      </div>
+    </section>
   )
 }
 
