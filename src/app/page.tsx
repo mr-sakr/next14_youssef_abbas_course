ab93f517e9a70601b4ad6bcec34120f87590cef9
sakr <sakr.bzns@gmail.com>
2024-02-22 00:14:08 +0200

    Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WebHostingPlan from "@/components/home/WebHostingPlan"
 const HomePage = () => {
   return (
     <section>
+      
       <Hero/>
       <h2 className="text-center mt-10 text-3xl font-bold">
         Choose Your Web Hosting Plan
